63961b3712d690976753fc09e4bfb73bdf682d76
Igor Akoulinine <igolinin@gmail.com>
2020-06-02 08:50:50 +0200

    hand in

--- Assets/GolfBall.cs
+++ Assets/GolfBall.cs
@@ -5,48 +5,51 @@ using UnityEngine;
 public class GolfBall : MonoBehaviour
 {
     
-    private new Rigidbody rigidbody;
-    public float mass = 0.0459f; // kg
+        
     [Header("Shot settings")]
     public float horizontalDegrees = 45f;
     public float verticalDegrees = 0f;
     public int RPM=7200;
-    public float ballSpeed;
-
+    [Range(-1, 1), Tooltip("-1 backspin,+1 topspin")]
+	public float backSpin;
+	[Range(-1, 1), Tooltip("-1 left, +1 right")]
+	public float sideSpin;
+    public float clubSpeed;
+    public float mass = 0.0459f; // kg
 
     public float smashFactor = 1.49f; // clubhead-to-ball-initial speed ratio
     public float crossSectionalArea; //m^2
     Vector3 position;
     public Vector3 velocity;
-    Vector3 acceleration;
-    Vector3 angularVelocity;
+    public Vector3 acceleration;
+    public Vector3 angularVelocity;
     private Vector3 startPos;
+    [Range(0, 1)]
+    public float restitutionCoeffient ;
+    
 
     [Header("Nature")]
     public float gravityMagnitude = -9.8f; // 9.8 m/s^2
     public float airDensity = 1.2041f; // kg/m^3
 
     
-    [Range(0, 1)]
-    public float restitutionCoeffient ;
+    
     
     private bool isIdle;
        
     [Header("Aerodynamics properties ")]
     public float dragCoefficient =  0.4f;
     public float liftCoefficient = 0.00001f ; // made this up?
+    public float magnusConstant = 0.03f;
     //////////////////////////////////////////////////////////////////////////    
-	[Header("Shot")]
 	
-	[Range(-1, 1), Tooltip("-1 backspin,+1 topspin")]
-	public float backSpin;
-	[Range(-1, 1), Tooltip("-1 left, +1 right")]
-	public float sideSpin;
+	
+	
 
     
 
 
-	public float magnusConstant = 0.03f;// Start is called before the first frame update
+	// Start is called before the first frame update
     ////////////////////////////////////////////////////////////////////////////////////
     private void Awake()
 	{
@@ -75,6 +78,7 @@ public class GolfBall : MonoBehaviour
                     Debug.Log("hit the ground");
                     acceleration = new Vector3(0,0,0);
                     velocity = new Vector3(0,0,0);
+                    angularVelocity =new Vector3(0,0,0);
                     transform.position = new Vector3(transform.position.x, 0 , transform.position.z);
                     isIdle = true;
                     
@@ -97,7 +101,7 @@ public class GolfBall : MonoBehaviour
         Vector3 currentVelocity = velocity;
         velocity = currentVelocity + acceleration*Time.deltaTime;
         transform.position = transform.position + velocity * Time.deltaTime;
-        //transform.Rotate (angularVelocity);
+        //transform.Rotate (angularVelocity*Time.deltaTime);
     }
     void getInitialVelocity(){
         velocity.y = Mathf.Sin(verticalDegrees * Mathf.PI / 180);
@@ -106,7 +110,7 @@ public class GolfBall : MonoBehaviour
         velocity.z = Mathf.Cos(verticalDegrees * Mathf.PI / 180);
         
         
-        velocity = velocity.normalized * ballSpeed*smashFactor;
+        velocity = velocity.normalized * clubSpeed*smashFactor;
     }
     void getInitialAngularVelocity(){
         var spin = new Vector3(backSpin, sideSpin, 0);
